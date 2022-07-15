471beff94d9bfa40e2efd5e0c7c586987a074a49
Melih Dokuzlar <melihdokuzlar71@gmail.com>
2022-07-15 22:25:14 +0300

    complated

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const app = express();
+const ejs = require('ejs');
+
+app.use(express.static('public'));
+
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
+app.get('/about', (req, res) => {
+  res.render('about');
+});
+
+app.get('/add_post', (req, res) => {
+  res.render('add_post');
+});
+
+app.get('/post', (req, res) => {
+  res.render('post');
+});
+
+const port = 3000;
+
+app.listen(port, () => {
+  console.log(`Server is create port : ${port} `);
+});
